99c73d5c6aef9d0848477fdb02d165e24d24e1ec
Calle Turesson <Digifreak@hotmail.se>
2018-10-12 15:49:54 +0200

    yeet

--- Index.php
+++ Index.php
@@ -1,5 +1,7 @@
 <?php 
+	$id = 1;
 	include_once 'header.php';
+	$title = "SELECT title FROM menu WHERE id = $id";
 ?>
 <section class="main-container">
 	<div class="main-wrapper">
@@ -7,6 +9,9 @@
 		<?php
 			if (isset($_SESSION['u_id'])) {
 				echo '<p class="p1">Välkommen!<p>';
+				echo '<a href="Index.php?menu id=1"><button type="button">banner</button></a>';
+				echo '<a href="Index.php?menu id=2"><button type="button">$title</button></a>';
+				echo '<textarea rows="4" cols="50">Hello World!</textarea>';
 			}
 			else {
 				echo '<h2>Home</h2>';
